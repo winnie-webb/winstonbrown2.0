351ca7470c13c678bd2abab1045e96458e9be68b
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-19 17:11:43 -0500

    Changed colors

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white fixed top-0 left-0 w-full shadow-md z-50">
+    <nav className="bg-gray-900 p-2  fixed top-0 left-0 w-full shadow-md z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
@@ -21,9 +21,7 @@ const Navbar = () => {
                 width={60}
                 height={60}
               ></Image>
-              <h1 className="text-2xl font-bold text-gray-900">
-                Winston Brown
-              </h1>
+              <h1 className="text-2xl font-bold text-white">Winston Brown</h1>
             </a>
           </div>
 
@@ -32,25 +30,25 @@ const Navbar = () => {
             <div className="ml-10 flex space-x-4">
               <a
                 href="#projects"
-                className="text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
+                className="text-white hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
               >
                 Projects
               </a>
               <a
                 href="#about"
-                className="text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
+                className="text-white hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
               >
                 About
               </a>
               <a
                 href="#skills"
-                className="text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
+                className="text-white hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
               >
                 Skills
               </a>
               <a
                 href="#contact"
-                className="text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
+                className="text-white hover:text-blue-500 px-3 py-2 rounded-md text-lg font-medium"
               >
                 Contact
               </a>
@@ -62,7 +60,7 @@ const Navbar = () => {
             <button
               onClick={toggleMenu}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-blue-500 focus:outline-none"
+              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-blue-500 focus:outline-none"
             >
               <svg
                 className="h-6 w-6"
@@ -97,19 +95,19 @@ const Navbar = () => {
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <a
               href="#projects"
-              className="block text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-base font-medium"
+              className="block text-white hover:text-blue-500 px-3 py-2 rounded-md text-base font-medium"
             >
               Projects
             </a>
             <a
               href="#about"
-              className="block text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-base font-medium"
+              className="block text-white hover:text-blue-500 px-3 py-2 rounded-md text-base font-medium"
             >
               About
             </a>
             <a
               href="#contact"
-              className="block text-gray-900 hover:text-blue-500 px-3 py-2 rounded-md text-base font-medium"
+              className="block text-white hover:text-blue-500 px-3 py-2 rounded-md text-base font-medium"
             >
               Contact
             </a>
